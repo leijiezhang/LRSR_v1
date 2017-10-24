502bf63f9d368eed9ed029a3c06384e0097ba2d2
leijiezhang <leijie.zhang@qq.com>
2017-10-24 20:58:27 +0800

    COILET rank plot's background is blank, but ohter s are with grid, so I add the grid on the plot

--- LRSR_result/afterwork/results - Yong/rank_sup.m
+++ LRSR_result/afterwork/results - Yong/rank_sup.m
@@ -1,6 +1,7 @@
 xlabel('Dimensionality of subspace','FontSize',20);
 ylabel('Rank','FontSize',20);
 title('');
+grid on;
 legend({'LRSR: Reduced rank mapping','SR: Full rank mapping'},'Location','NorthWest');
 set(gca,'FontSize',20);
 print COIL20_rank_sup.eps -depsc2 -r600
